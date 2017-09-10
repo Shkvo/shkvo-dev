b0b4a761be0420d7fc0b3e3ab73af490c3296ae1
Dima Bazanov <dimashkvo@gmail.com>
2017-09-10 11:21:18 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './Shkvo.svg';
+import { Link ,Switch, Route } from 'react-router-dom';
+import greet from './logot.svg';
+import './css/keyframes.css';
+import './css/fonts.css';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+		return (
+			<div className="App">
+					<object id="f1" data={logo} type="image/svg+xml" aria-label="logo"></object>
+			</div>
+		);
+  }
+}
+
+export default App;
