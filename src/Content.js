4c918b68c96fde29859d6e990d33ffa07cf8eb63
Dima Bazanov <dimashkvo@gmail.com>
2017-09-13 23:32:26 +0300

    Added some content for 1st block

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './css/Content.css';
+import beardMan from './img/beard-man.jpg'
 
 const Content = () => (
     <section>
-        
+        <div className="about-block">
+            <div className="beard-man">
+                <img src={beardMan} alt="a" />
+                <div className="beard-man-overlay"></div>
+            </div>
+            <div className="about-text">
+                <h1>Hi, i'm Dima,</h1>
+                <span>a frontend developer.</span>
+            </div>
+        </div>
     </section>
 )
 
